12cfbad4ac9ed5c0803a8ae049da5b0a61629f90
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-08-10 14:02:12 +0300

    new fix

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const mainContent = document.getElementById("main-content");
 
 //making variables for the current day and the last day of the year.
 let currentDate = new Date();
-let lastDate = new Date("12/31/2021");
+const lastDate = new Date("12/31/2021");
 //taking the dates minus each other.
-let inDays;
-let inHours;
-let inMinutes;
-let inSecunds;
 
-inDays = Math.floor(differenceInMilliseconds / (1000 * 3600 * 24))
+inDays = Math.floor((lastDate - currentDate) / (1000 * 3600 * 24)) + " dagar"
 
-console.log(inDays);
+mainContent.innerHTML = inDays;
